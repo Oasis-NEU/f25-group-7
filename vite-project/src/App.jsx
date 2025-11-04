2d01f11e86de000e1823114970ee2ce42e91724a
Shlok Patel <patel.s15@northeastern.edu>
2025-11-04 18:04:46 -0500

    commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,9 @@ import Login from './Login.jsx'
 function App() {
 
   return (
-    <div>
+    <div className='bg-[#121321] w-screen h-screen 
+    flex justify-center items-center'>
+      <h1 className='text-black'>Shlok Patel</h1>
     <Login />
     </div>
   )
