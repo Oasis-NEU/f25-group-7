60665033099349e3f65d3752fb4e3a1a97c0de71
Yash Maheshwari <ymaheshwari2007@gmail.com>
2025-11-09 15:19:03 -0500

    added routes so home page is default, /login leads to login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from './pages/Login';
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route index element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>} />
+        <Route path= "*" element={<NotFound/>}></Route>
       </Routes>
     </Router>
   );
